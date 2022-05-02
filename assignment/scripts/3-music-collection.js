2249ee224385fc0926a9ed316008c377a7d28d86
Andrew Leck <Aleck360@gmail.com>
2022-05-02 02:56:07 -0500

    working on first few questions

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,8 +1,22 @@
 console.log('***** Music Collection *****')
-let Collection = [];
 
-function addToCollection(title, artist, yearPublished){
+//- Add a function named `addToCollection`. This function should:
+  //- Take in the album's `title`, `artist`, `yearPublished` as input parameters
+  //- Create a new object having the above properties
+  //- Add the new object to the end of the `collection` array
+  //- Return the newly created object
 
 
+
+let collection = [];
+let album = new Object();
+
+function addToCollection(Object){
+    album.title = 'Donda';
+    album.artist = 'Kanye';
+    album.yearPublished = 2021;
+    collection.push(Object);
+    return album;
 }
-console.log( addToCollection() );
+
+console.log('Testing', addToCollection( collection));
