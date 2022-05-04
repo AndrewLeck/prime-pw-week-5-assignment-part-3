0d73063f3bbca529c1d27cee83f5443cc8b33e4c
Andrew Leck <Aleck360@gmail.com>
2022-05-04 17:27:55 -0500

    completed rest of questions ' git status
    ls
    git status
    '

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,16 +7,73 @@ console.log('***** Music Collection *****')
   //- Return the newly created object
 
 
-
 let collection = [];
-let album = new Object();
-
-function addToCollection(Object){
-    album.title = 'Donda';
-    album.artist = 'Kanye';
-    album.yearPublished = 2021;
-    collection.push(Object);
-    return album;
+function addToCollection(title, artist, yearPublished){
+let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(album);
+   return album;
 }
 
-console.log('Testing', addToCollection( collection));
+//- Test the `addToCollection` function:
+  //- Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+  //- Console.log each album as added using the returned value.
+ // - After all are added, console.log the `collection` array.
+
+ console.log('Testing', addToCollection( 'Donda', 'Kanye', 2021));
+ console.log('Testing', addToCollection( 'Ye', 'Kanye', 2018));
+ console.log('Testing', addToCollection( 'The College Dropout', 'Kanye', 2004));
+ console.log('Testing', addToCollection( 'Abbey Road', 'The Beatles', 1969));
+ console.log('Testing', addToCollection( 'Fearless', 'Taylor Swift', 2008));
+ console.log('Testing', addToCollection( '17', 'XXXTENTACION', 2017));
+ console.log(collection); 
+
+
+ //- Add a function named `showCollection`. This function should:
+  //- Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+ // - Console.log the number of items in the array.
+ // - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
+ function showCollection(array){
+  console.log('my array has this many items in it',array.length);
+  for(let i=0; i <= array.length-1; i++){
+    if(array[i] === array[i]){
+      console.log(array[i].title,'by', array[i].artist,'published in', array[i].yearPublished);
+    }
+  }
+}
+showCollection(collection);
+
+
+//- Add a function named `findByArtist`. This function should:
+ // - Take in `artist` (a string) parameter
+  //- Create an array to hold any results, empty to start
+  //- Loop through the `collection` and add any objects with a matching artist to the array.
+//  - Return the array with the matching results. If no results are found, return an empty array.
+
+
+
+
+
+let results =[];
+function findByArtist(artist){
+  for(let i=0; i <= collection.length-1; i++){
+    if(collection[i].artist === collection[i].artist){
+      results.push(collection[i].artist);
+        return results;
+    }
+        return false;
+  }
+}
+//Testing
+findByArtist('Kanye');
+findByArtist('Taylor Swift');
+findByArtist('John doe');
+findByArtist('John doe');
+findByArtist('John doe');
+findByArtist('John doe');
+
+
+console.log(results);
